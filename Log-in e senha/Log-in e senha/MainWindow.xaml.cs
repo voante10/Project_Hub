9a404e8f2cb041160786f2c03250b6b2a090780c
Bruno <santanamourab09@gmail.com>
2024-07-26 14:45:04 -0300

    1v
    falta muito ainda, mas da pra inserir a logica no proximo coding

---
+++ Log-in e senha/Log-in e senha/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Log_in_e_senha
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        // Cancel button :)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Exit();
+        }
+        public void Exit()
+        {
+            Close();
+        }
+    }
+    
+}
